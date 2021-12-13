769bd6aed1e5fd2ced9665f85be8be65361bcda1
git-afsantos <contact.andre.santos@gmail.com>
2021-12-13 14:39:59 +0000

    formatting

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@
 ###############################################################################
 
 from pathlib import Path
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 ###############################################################################
 # Constants
