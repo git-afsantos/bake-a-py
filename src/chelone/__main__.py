10c3b9789447e5bc6fac380f97d2803aee7aeb7e
git-afsantos <contact.andre.santos@gmail.com>
2021-12-14 15:15:24 +0000

    linting

--- src/chelone/__main__.py
+++ src/chelone/__main__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2021 André Santos
 
-'''
+"""
 Entrypoint module, in case you use `python -m <package>`.
 
 Why does this file exist, and why __main__? For more info, read:
@@ -9,7 +9,7 @@ Why does this file exist, and why __main__? For more info, read:
   - https://www.python.org/dev/peps/pep-0338/
   - https://docs.python.org/2/using/cmdline.html#cmdoption-m
   - https://docs.python.org/3/using/cmdline.html#cmdoption-m
-'''
+"""
 
 ###############################################################################
 # Imports
@@ -23,5 +23,5 @@ from chelone.cli import main
 # Entry Point
 ###############################################################################
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))  # pragma: no cover
--- src/chelone/cli.py
+++ src/chelone/cli.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2021 André Santos
 
-'''
+"""
 Module that contains the command line program.
 
 Why does this file exist, and why not put this in __main__?
@@ -9,14 +9,15 @@ Why does this file exist, and why not put this in __main__?
   In some cases, it is possible to import `__main__.py` twice.
   This approach avoids that. Also see:
   https://click.palletsprojects.com/en/5.x/setuptools/#setuptools-integration
-'''
+"""
 
 ###############################################################################
 # Imports
 ###############################################################################
 
-import argparse
 from typing import Any, Dict, List
+
+import argparse
 
 from chelone import __version__ as current_version
 
@@ -24,15 +25,18 @@ from chelone import __version__ as current_version
 # Argument Parsing
 ###############################################################################
 
+
 def parse_arguments(args: List[str]) -> Dict[str, Any]:
     msg = 'A short description of the project.'
     parser = argparse.ArgumentParser(description=msg)
 
-    parser.add_argument('--version', dest='version', action='store_true',
-                        help='Prints the program version.')
+    parser.add_argument(
+        '--version', dest='version', action='store_true', help='Prints the program version.'
+    )
 
-    parser.add_argument('args', metavar='ARG', nargs=argparse.ZERO_OR_MORE,
-                        help='An argument for the program.')
+    parser.add_argument(
+        'args', metavar='ARG', nargs=argparse.ZERO_OR_MORE, help='An argument for the program.'
+    )
 
     args = parser.parse_args(args=args)
     args = vars(args)
@@ -42,6 +46,7 @@ def parse_arguments(args: List[str]) -> Dict[str, Any]:
 ###############################################################################
 # Entry Point
 ###############################################################################
+
 
 def main(argv: List[str]) -> int:
     args = parse_arguments(argv)
